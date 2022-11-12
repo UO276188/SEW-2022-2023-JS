02690c1532bc3ba590779a38eb88b1cef26daeea
Sara María Ramírez Pérez <79209417+UO276188@users.noreply.github.com>
2022-11-12 11:43:26 +0100

    Ej 2

---
+++ Ejercicio-02/MasInfoNavegador.js
@@ -0,0 +1,10 @@
+document.write("Versión: ");
+document.write(infoNavegador.version);
+document.write(" - Plataforma: ");
+document.write(infoNavegador.plataforma);
+document.write(" - Vendedor: ");
+document.write(infoNavegador.vendedor);
+document.write(" - Agente: ");
+document.write(infoNavegador.agente);
+document.write(" - Java activo: ");
+document.write(infoNavegador.javaActivo);
