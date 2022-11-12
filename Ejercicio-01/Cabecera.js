0bdb8af053daae930f96bd969f36f8bb2a706a5f
Sara María Ramírez Pérez <79209417+UO276188@users.noreply.github.com>
2022-11-12 11:32:49 +0100

    Ej 1

---
+++ Ejercicio-01/Cabecera.js
@@ -0,0 +1,8 @@
+var asignatura = new Object();
+asignatura.nombre = "Software y Estándares para la Web";
+asignatura.titulacion = "Grado en Ingeniería Informática del Software";
+asignatura.centro = "Escuela de Ingeniería Informática";
+asignatura.universidad = "Universidad de Oviedo";
+asignatura.curso = "2022-2023";
+asignatura.estudiante = "Sara María Ramírez Pérez";
+asignatura.email = "uo276188@uniovi.es";
