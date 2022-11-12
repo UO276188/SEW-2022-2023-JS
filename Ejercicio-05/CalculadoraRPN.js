8c9fa71506522e0359cc60eeb86065c5db3e2f6e
Sara María Ramírez Pérez <79209417+UO276188@users.noreply.github.com>
2022-11-12 20:20:48 +0100

    Ej5

---
+++ Ejercicio-05/CalculadoraRPN.js
@@ -0,0 +1,234 @@
+"use strict";
+class PilaLIFO {
+    constructor() {
+        this.pila = new Array();
+    }
+    apilar(valor) {
+        this.pila.push(valor);
+    }
+
+    desapilar() {
+        return (this.pila.pop());
+    }
+
+    show(){
+        var res = "";
+        var size = this.size();
+        for (var i in this.pila)
+            res += size-- + "\t\t" + this.pila[i]+"\n";
+
+        return res;
+    } 
+
+    limpiar(){
+        var size = this.size();
+        for (var i = 0; i < size; i++){
+            this.pila.pop();
+        }
+    }
+
+    size() {
+        return this.pila.length;
+    }
+    
+}
+class CalculadoraRPN{
+
+    constructor() {
+        this.prepantalla = "";
+        this.pila = new PilaLIFO();
+
+        this.inicializar();
+    }
+
+
+    inicializar(){
+        document.addEventListener('keydown', function (event) {
+            if (event.key >= '0' && event.key <= '9') {
+                calculadora.digitos(event.key);
+            }
+            if (event.key === 'Enter') {
+                calculadora.enter();
+            }
+            
+            
+            if (event.key === '/') {
+                calculadora.division();
+            }
+            if (event.key === '*') {
+                calculadora.multiplicacion();
+            }
+            if (event.key === '-') {
+                calculadora.resta();
+            }
+            if (event.key === '+') {
+                calculadora.suma();
+            }
+            if (event.key === '.') {
+                calculadora.punto();
+            }
+        
+            
+            if (event.key === 'Delete') { // Supr
+                calculadora.borrar(); //C
+            }
+            if (event.key === 'Backspace') {
+                calculadora.CE();
+            }
+
+        });    
+    }
+
+    
+    digitos(n){
+        this.prepantalla += n;
+        this.mostrarPrePantalla();
+    }
+
+    punto(){
+        this.prepantalla += ".";
+        this.mostrarPrePantalla();
+    }
+
+
+    // OPERACIONES ----------------------------------------------------
+
+    suma(){
+        if (this.pila.size() >= 2){
+            var op2 = this.pila.desapilar();
+            var op1 = this.pila.desapilar();
+            var result = parseFloat(op1) + parseFloat(op2);
+            this.pila.apilar(result);
+
+            this.mostrarPantalla();
+        }        
+    }
+
+    resta(){
+        if (this.pila.size() >= 2){
+            var op2 = this.pila.desapilar();
+            var op1 = this.pila.desapilar();
+            var result = parseFloat(op1) - parseFloat(op2);
+            this.pila.apilar(result);
+
+            this.mostrarPantalla();
+        }        
+    }
+
+    multiplicacion(){
+        if (this.pila.size() >= 2){
+            var op2 = this.pila.desapilar();
+            var op1 = this.pila.desapilar();
+            var result = parseFloat(op1) * parseFloat(op2);
+            this.pila.apilar(result);
+
+            this.mostrarPantalla();
+        }
+    }
+
+    division(){
+        if (this.pila.size() >= 2){
+            var op2 = this.pila.desapilar();
+            var op1 = this.pila.desapilar();
+            var result = parseFloat(op1) / parseFloat(op2);
+            this.pila.apilar(result);
+
+            this.mostrarPantalla();
+        }
+    }
+
+    sin(){
+        if (this.pila.size() >= 1){
+            var op = this.pila.desapilar();
+            var res = Math.sin(op);
+            this.pila.apilar(result);
+
+            this.mostrarPantalla();
+        }
+    }
+
+    cos(){
+        if (this.pila.size() >= 1){
+            var op = this.pila.desapilar();
+            var res = Math.cos(op);
+            this.pila.apilar(result);
+
+            this.mostrarPantalla();
+        }
+    }
+
+    tan(){
+        if (this.pila.size() >= 1){
+            var op = this.pila.desapilar();
+            var res = Math.tan(op);
+            this.pila.apilar(result);
+
+            this.mostrarPantalla();
+        }
+    }
+
+    arcoSeno(){
+        if (this.pila.size() >= 1){
+            var op = this.pila.desapilar();
+            var res = Math.asin(op);
+            this.pila.apilar(result);
+
+            this.mostrarPantalla();
+        }
+    }
+
+    arcoCoseno(){
+        if (this.pila.size() >= 1){
+            var op = this.pila.desapilar();
+            var res = Math.acos(op);
+            this.pila.apilar(result);
+
+            this.mostrarPantalla();
+        }
+    }
+
+    arcoTangente(){
+        if (this.pila.size() >= 1){
+            var op = this.pila.desapilar();
+            var res = Math.atan(op);
+            this.pila.apilar(result);
+
+            this.mostrarPantalla();
+        }
+    }
+
+
+
+    borrar(){
+        this.CE();
+        this.pila.limpiar();
+        this.mostrarPantalla();
+    }
+
+    CE(){ //limpiar valores a introducir
+        this.prepantalla = "";
+        this.mostrarPrePantalla();
+    }
+
+
+
+
+
+    enter(){
+        if (this.prepantalla != ""){
+            this.pila.apilar(this.prepantalla);
+            this.CE();
+            this.mostrarPantalla();
+        }
+    }
+
+    mostrarPantalla(){
+        document.querySelector('body > form > textarea[title="pantalla"]').value = this.pila.show();
+    }
+    mostrarPrePantalla(){
+        document.querySelector('body > form > textarea[title="prePantalla"]').value = this.prepantalla;
+    }
+}
+
+var calculadora = new CalculadoraRPN();
+
