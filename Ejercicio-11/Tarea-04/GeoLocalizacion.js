8366d1574886355e278229187981ed0393b1bda1
Sara María Ramírez Pérez <79209417+UO276188@users.noreply.github.com>
2022-12-06 18:27:22 +0100

    Delete Ejercicio-11 directory

--- Ejercicio-11/Tarea-04/GeoLocalizacion.js
+++
@@ -1,8 +0,0 @@
-var mapaDinamicoGoogle = new Object();
-function initMap(){
-    
-    var aviles = {lat: 43.5547300, lng: -5.9248300};
-    var mapaAviles = new google.maps.Map( document.querySelector('body > main'),{zoom: 15,center:aviles});
-    var marcador = new google.maps.Marker({position:aviles,map:mapaAviles});
-}
-mapaDinamicoGoogle.initMap = initMap;
